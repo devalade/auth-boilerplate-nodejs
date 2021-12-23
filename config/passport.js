15ac5468b10573a1b0fcafa6b9680c8cf2fb4d37
devalade <aladecharaf23@gmail.com>
2021-12-23 14:37:42 +0100

    feat: fix some bug

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,15 @@
 const fs = require("fs");
 const path = require("path");
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-var GitHubStrategy = require('passport-github2').Strategy;
-const User = require('../models').user;
+const GitHubStrategy = require("passport-github2").Strategy;
+const User = require("../models").user;
 
+require("dotenv").config();
 
-require('dotenv').config();
-
-const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
-const PUB_KEY = fs.readFileSync(pathToKey, 'utf-8');
-
+const pathToKey = path.join(__dirname, "..", "id_rsa_pub.pem");
+const PUB_KEY = fs.readFileSync(pathToKey, "utf-8");
 
 /**
  * ------------- STRATEDIES OPTION ------------------
@@ -19,128 +17,131 @@ const PUB_KEY = fs.readFileSync(pathToKey, 'utf-8');
 
 // JWT Option
 const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: PUB_KEY,
-    algorithms: ['RS256']
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ["RS256"],
 };
 // Google Option
 const googleOption = {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/users/auth/google/redirect" // FIXME: Check if the url is working 
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/users/auth/google/redirect", // FIXME: Check if the url is working
 };
 // Github Option
 const githubOption = {
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/users/auth/github/callback"
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/users/auth/github/callback",
 };
 // Facebook Option
 const facebookOption = {
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:5000/users/auth/facebook/callback"
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:5000/users/auth/facebook/callback",
 };
 
 /**
  * ------------- STRATEGIES ------------------
  */
 
-// JWT strategy 
-const strategy = new JwtStrategy(
-    jwtOptions,
-    (payload, done) => {
-        console.log(payload);
-        User.findOne({
-            where: {
-                id: payload.sub
-            }
-        }).then((user) => {
-            if (user) {
-                return done(null, user);
-            } else {
-                return done(null, false);
-            }
-        })
-            .catch(err => done(err, null))
-    });
+// JWT strategy
+const strategy = new JwtStrategy(jwtOptions, (payload, done) => {
+  console.log(payload);
+  User.findOne({
+    where: {
+      id: payload.sub,
+    },
+  })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => done(err, null));
+});
 
-// Google strategy  
+// Google strategy
 const googleStrategy = new GoogleStrategy(
-    googleOption,
-    (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        const defaultUser = {
-            'firstname': profile.name.givenName,
-            'lastname': profile.name.familyName,
-            'email': profile.emails[0].value,
-            'password': 'password',
-            'username': profile.username,
-            'googeId': profile.id
-        }
-    
-        User.findOrCreate({
-            where: { googleId: profile.id },
-            defaults: defaultUser
-        }).then((user) => {
-            console.log(user);
-            return done(null, user[0]);
-        })
-            .catch((error) => done(error, null))
-    });
+  googleOption,
+  (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+    const defaultUser = {
+      firstname: profile.name.givenName,
+      lastname: profile.name.familyName,
+      email: profile.emails[0].value,
+      password: "password",
+      username: profile.username,
+      googeId: profile.id,
+    };
+
+    User.findOrCreate({
+      where: { googleId: profile.id },
+      defaults: defaultUser,
+    })
+      .then((user) => {
+        console.log(user);
+        return done(null, user[0]);
+      })
+      .catch((error) => done(error, null));
+  }
+);
 
 // Github strategy
 const githubStrategy = new GitHubStrategy(
-    githubOption,
-    (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        const defaultUser = {
-            'firstname': profile.displayName,
-            'lastname': "",
-            'email': profile.emails[0].value,
-            'password': 'password',
-            'username': profile.username,
-            'githubId': profile.id
-        }
-    
-        User.findOrCreate({
-            where: { githubId: profile.id },
-            defaults: defaultUser
-        }).then((user) => {
-            console.log(user);
-            return done(null, user[0]);
-        })
-            .catch((error) => done(error, null))
-    }
+  githubOption,
+  (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+    const defaultUser = {
+      firstname: profile.displayName,
+      lastname: "",
+      email: profile.emails[0].value,
+      password: "password",
+      username: profile.username,
+      githubId: profile.id,
+    };
+
+    User.findOrCreate({
+      where: { githubId: profile.id },
+      defaults: defaultUser,
+    })
+      .then((user) => {
+        console.log(user);
+        return done(null, user[0]);
+      })
+      .catch((error) => done(error, null));
+  }
 );
 
 // facebook strategy
-const facebookStrategy = new FacebookStrategy(
-    facebookOption,
-    (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        const defaultUser = {
-            'firstname': profile.displayName,
-            'lastname': "",
-            'email': profile.emails[0].value,
-            'password': 'password',
-            'username': profile.username,
-            'githubId': profile.id
-        }
-    
-        User.findOrCreate({
-            where: { githubId: profile.id },
-            defaults: defaultUser
-        }).then((user) => {
-            console.log(user);
-            return done(null, user[0]);
-        })
-            .catch((error) => done(error, null))
-    }
-);
+// const facebookStrategy = new FacebookStrategy(
+//   facebookOption,
+//   (accessToken, refreshToken, profile, done) => {
+//     console.log(profile);
+//     const defaultUser = {
+//       firstname: profile.displayName,
+//       lastname: "",
+//       email: profile.emails[0].value,
+//       password: "password",
+//       username: profile.username,
+//       githubId: profile.id,
+//     };
+
+//     User.findOrCreate({
+//       where: { githubId: profile.id },
+//       defaults: defaultUser,
+//     })
+//       .then((user) => {
+//         console.log(user);
+//         return done(null, user[0]);
+//       })
+//       .catch((error) => done(error, null));
+//   }
+// );
 module.exports = (passport) => {
-    passport.use('jwt', strategy);
-    passport.use('google', googleStrategy);
-    passport.use('github', githubStrategy);
-    passport.use('facebook', facebookStrategy);
-}
+  passport.use("jwt", strategy);
+  passport.use("google", googleStrategy);
+  passport.use("github", githubStrategy);
+  //   passport.use("facebook", facebookStrategy);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,90 +1,141 @@
-const router = require('express').Router();
-const passport = require('passport');
-const utils = require('../lib/utils');
+const router = require("express").Router();
+const passport = require("passport");
+const utils = require("../lib/utils");
 const Users = require("../models").user;
 
 // TODO
-router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+router.get(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
     res.status(200).json({ success: true, message: "You are authorized!" });
-});
+  }
+);
 
 // TODO
-router.post('/login', (req, res, next) => {
-    Users.findOne({
-        where: { username: req.body.username }
-    }).then((user) => {
-        if (!user) {
-            res.status(401).json({ success: false, message: "Could not find user" })
-        }
+router.post("/login", (req, res, next) => {
+  Users.findOne({
+    where: { username: req.body.username },
+  })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(401)
+          .json({ success: false, message: "Could not find user" });
+      }
 
-        const isValid = utils.validPassword(req.body.password, user.password, user.salt);
+      const isValid = utils.validPassword(
+        req.body.password,
+        user.password,
+        user.salt
+      );
 
-        if (isValid) {
-            const { token, expires } = utils.issueJWT(user);
+      if (isValid) {
+        const { token, expires } = utils.issueJWT(user);
 
-            res.status(200).json({ success: true, user: user, token: token, expiresIn: expires })
-        } else {
-            res.status(401).json({ success: false, message: "You entered a wrong password" });
-        }
-    }).catch((err) => next(err));
+        res.status(200).json({
+          success: true,
+          user: user,
+          token: token,
+          expiresIn: expires,
+        });
+      } else {
+        res
+          .status(401)
+          .json({ success: false, message: "You entered a wrong password" });
+      }
+    })
+    .catch((err) => next(err));
 });
 
 //TODO
-router.post('/register', (req, res, next) => {
-    const { firstname, lastname, username, password } = req.body
-    const saltHash = utils.genPassword(password);
+router.post("/register", (req, res, next) => {
+  const { firstname, lastname, username, password } = req.body;
+  const saltHash = utils.genPassword(password);
 
-    const salt = saltHash.salt;
-    const hash = saltHash.hash;
-    
-    // FIXME: Move the create function to services
-    Users.create({
-        username,
-        firstname,
-        lastname,
-        password: hash,
-        salt: salt
-    }).then((user) => {
-        const { token, expires } = utils.issueJWT(user);
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
 
-        res.json({ success: true, user: user, token: token, expiresIn: expires });
-    });
+  // FIXME: Move the create function to services
+  Users.create({
+    username,
+    firstname,
+    lastname,
+    password: hash,
+    salt: salt,
+  }).then((user) => {
+    const { token, expires } = utils.issueJWT(user);
+
+    res.json({ success: true, user: user, token: token, expiresIn: expires });
+  });
 });
 
-router.get("/auth/google", passport.authenticate("google", {
-    scope: ["profile", "email"]
-}));
-  
-router.get("/auth/google/redirect",
-    passport.authenticate('google', { failureRedirect: '/login', session: false }), (req, res) => {
-        console.log("User:", req.user);
-        const { token, expires } = utils.issueJWT(req.user);
+// router.get(
+//   "/auth/google",
+//   passport.authenticate("google", {
+//     scope: ["profile", "email"],
+//   })
+// );
 
-        res.json({ success: true, user: req.user, token: token, expiresIn: expires });
+// router.get(
+//   "/auth/google/redirect",
+//   passport.authenticate("google", {
+//     failureRedirect: "/login",
+//     session: false,
+//   }),
+//   (req, res) => {
+//     console.log("User:", req.user);
+//     const { token, expires } = utils.issueJWT(req.user);
+
+//     res.json({
+//       success: true,
+//       user: req.user,
+//       token: token,
+//       expiresIn: expires,
+//     });
+//   }
+// );
+
+router.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/login",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("User:", req.user);
+    const { token, expires } = utils.issueJWT(req.user);
+
+    res.json({
+      success: true,
+      user: req.user,
+      token: token,
+      expiresIn: expires,
     });
+  }
+);
 
-router.get('/auth/github',passport.authenticate('github', { scope: [ 'user:email' ] }));
+router.get("/auth/facebook", passport.authenticate("facebook"));
 
-router.get("/auth/github/callback",
-    passport.authenticate('github', { failureRedirect: '/login', session: false }),
-    (req, res) => {
-        console.log("User:", req.user);
-        const { token, expires } = utils.issueJWT(req.user);
+router.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  (req, res) => {
+    console.log("User:", req.user);
+    const { token, expires } = utils.issueJWT(req.user);
 
-        res.json({ success: true, user: req.user, token: token, expiresIn: expires });
+    res.json({
+      success: true,
+      user: req.user,
+      token: token,
+      expiresIn: expires,
     });
-
-router.get('/auth/facebook',
-    passport.authenticate('facebook'));
-
-router.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
-    (req, res) => {
-        console.log("User:", req.user);
-        const { token, expires } = utils.issueJWT(req.user);
-
-        res.json({ success: true, user: req.user, token: token, expiresIn: expires });
-    });
-
+  }
+);
 
 module.exports = router;
